8624ecb87eddcb31205e8c075098d44ad23fb991
Bhoomika <bhoomigowda36@gmail.com>
2026-04-23 08:09:24 +0530

    Day 2 — React frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Multi-Language Support Engine</h1>
+      <h2>Day 2 Frontend Setup Complete 🚀</h2>
+    </div>
+  );
+}
+
+export default App;
